2ca0ec908ca76b8bdb3014d41d5d77cbf3e8e363
Daniel-Llanda <east.danielteddyllandaa@gmail.com>
2023-05-24 20:57:00 -0700

    laravel making
    maganda si gewn

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>EVENTS PAGE</title>
-</head>
-<body>
-    <header>
-        <nav>
-            <a href="">LANDING PAGE</a>
-            <a href="">HOME</a>
-            <a href="">PROFILE</a>
-            <a href="">EVENTS</a>
-            <a href="">NEWS</a>
-            <a href="">CONTACT</a>
-        </nav>
-    </header>
-    <h1>WELCOME TO EVENT PAGE</h1>
-</body>
-</html>
+@extends('layout.app')
+
+@section('content')
+<h1>WELCOME TO EVENT PAGE</h1>
+@endsection
+
+@section('title')
+EVENT PAGE
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing');
-});
+})->name('landing');
 
 Route::get('/home', function(){
     return view('home');
-});
+})->name('home');
+
 Route::get('/profile', function(){
     return view('profile');
-});
+})->name('profile');
+
 Route::get('/event', function(){
     return view('event');
-});
+})->name('event');
+
 Route::get('/news', function(){
     return view('news');
-});
+})->name('news');
+
 Route::get('/contacts', function(){
-    return view('contact');
-});
+    return view('contacts');
+})->name('contacts');
